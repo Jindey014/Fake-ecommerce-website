b4fc4d5eddf02f928f3417347bac4a566da9093e
Jinish Shrestha <dixon.cider014@gmail.com>
2023-05-08 17:45:02 +0545

    Ecommerce website

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './containers/Header'
+import ProductListing from './containers/ProductListing'
+import ProductDetails from './containers/ProductDetails'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing />}></Route>
+          <Route
+            path="/product/:productId"
+            element={<ProductDetails />}
+          ></Route>
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
